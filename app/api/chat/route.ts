04809f1e4a23aa11e9295d4f89173917812aeef6
Miguel Ordoñez <miguel.ordonez@ipc.com>
2025-08-14 12:20:14 +0100

    (refactor) call the tools until getting completion

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,6 @@
 import { MessagePayload } from "@/app/types/definitions";
-import { llm } from "@/lib/llm";
+import { llmWithTools } from "@/lib/llm";
+import { toolsByName } from "@/lib/tools";
 import { AIMessage, BaseMessage, HumanMessage } from "@langchain/core/messages";
 import type { NextRequest } from "next/server";
 
@@ -14,6 +15,35 @@ const toBaseMessage = (payload: MessagePayload): BaseMessage => {
   }
 };
 
+const getCompletion = async (
+  messages: BaseMessage[],
+  maxSteps = 3
+): Promise<AIMessage> => {
+  let steps = 0;
+  let history = [...messages];
+
+  do {
+    const aiMessage = await llmWithTools.invoke(history);
+
+    // If the AI message doesn't have any tool calls, return it
+    if (!aiMessage.tool_calls || aiMessage.tool_calls.length === 0) {
+      return aiMessage;
+    }
+
+    // If the AI message has tool calls, add it to the history and call the tools
+    history = [...history, aiMessage];
+    for (const toolCall of aiMessage.tool_calls) {
+      const selectedTool =
+        toolsByName[toolCall.name as keyof typeof toolsByName];
+      const toolMessage = await selectedTool.invoke(toolCall);
+      history = [...history, toolMessage];
+    }
+    steps += 1;
+  } while (steps < maxSteps);
+
+  return new AIMessage("Reached tool call limit without final answer.");
+};
+
 // TODO: Add type for the request body
 // TODO: Summarize the conversation history (https://js.langchain.com/docs/concepts/chat_history/)
 export async function POST(request: NextRequest) {
@@ -21,6 +51,6 @@ export async function POST(request: NextRequest) {
   const messagePayloads = body.messagePayloads as MessagePayload[];
   const messages = messagePayloads.map(toBaseMessage);
 
-  const completion = await llm.invoke(messages);
-  return Response.json({ content: completion.content });
+  const aiMessage = await getCompletion(messages);
+  return Response.json({ content: aiMessage.content });
 }
