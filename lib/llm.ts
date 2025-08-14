42a6a28099c2be546b9e3bcbdd3a03f8b0ff7b13
Miguel Ordoñez <miguel.ordonez@ipc.com>
2025-08-14 12:19:15 +0100

    (feat) add calculator tool

--- lib/llm.ts
+++ lib/llm.ts
@@ -1,7 +1,10 @@
 import { ChatOpenAI } from "@langchain/openai";
+import { toolsByName } from "./tools";
 
-export const llm = new ChatOpenAI({
+const llm = new ChatOpenAI({
   model: "gpt-3.5-turbo",
   temperature: 0,
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+export const llmWithTools = llm.bindTools(Object.values(toolsByName));
