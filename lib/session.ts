489dde629134ef9c6dec6a71ed861d722ee9cdee
Miguel Ordoñez <miguel.ordonez@ipc.com>
2025-08-20 16:21:28 +0100

    (feat) create session for the user

---
+++ lib/session.ts
@@ -0,0 +1,47 @@
+import "server-only";
+
+import { JWTPayload, SignJWT, jwtVerify } from "jose";
+import { cookies } from "next/headers";
+// import { SessionPayload } from "@/app/lib/definitions";
+
+const secretKey = process.env.SESSION_SECRET;
+const encodedKey = new TextEncoder().encode(secretKey);
+
+export const encrypt = async (payload: JWTPayload) => {
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("1h")
+    .sign(encodedKey);
+};
+
+export const decrypt = async (session: string = "") => {
+  try {
+    const { payload } = await jwtVerify(session, encodedKey, {
+      algorithms: ["HS256"],
+    });
+    return payload;
+  } catch (error) {
+    console.log(`Failed to verify session: ${error}`);
+  }
+};
+
+export async function createSession(payload: JWTPayload) {
+  const oneHourInMilliseconds = 1 * 60 * 60 * 1000;
+  const expiresAt = new Date(Date.now() + oneHourInMilliseconds);
+  const session = await encrypt(payload);
+  const cookieStore = await cookies();
+
+  cookieStore.set("session", session, {
+    httpOnly: true,
+    secure: true,
+    expires: expiresAt,
+    sameSite: "lax",
+    path: "/",
+  });
+}
+
+export async function deleteSession() {
+  const cookieStore = await cookies();
+  cookieStore.delete("session");
+}
